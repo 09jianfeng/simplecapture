1cae184215d46c0f72f23e0d9718de82b1436ec7
chenjianfeng <chenjianfeng@youmi.net>
2017-04-26 15:59:46 +0800

    设置为1秒10帧

--- SimpleCapture/VideoOutput/YUVFileTransform.m
+++ SimpleCapture/VideoOutput/YUVFileTransform.m
@@ -74,7 +74,7 @@
     
     
     _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, _readFileQueue);
-    dispatch_source_set_timer(_timer, DISPATCH_TIME_NOW, 0.04 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_timer(_timer, DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
     dispatch_source_set_event_handler(_timer, ^{
         NSData *yuvData = [self.yuvfilere readOneFrameYUVDataWithFile:inputFile error:nil];
         if (yuvData && yuvData.length > 0) {
